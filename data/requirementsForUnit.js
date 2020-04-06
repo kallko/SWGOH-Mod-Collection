da1bcc2cd516393a87c4df525f74d67221eb6c73
Andrii Kalko <andrii@casavi.de>
2020-04-06 17:51:22 +0200

    A-0: Move requirements data to separate file      Add prettier formater

---
+++ data/requirementsForUnit.js
@@ -0,0 +1,116 @@
+const KAYLO_REQ_UNITS = [
+  {
+    base_id: "KYLORENUNMASKED",
+    power: 30429,
+  },
+  {
+    base_id: "FIRSTORDERTROOPER",
+    power: 23248,
+  },
+  {
+    base_id: "FIRSTORDEROFFICERMALE",
+    power: 21052,
+  },
+  {
+    base_id: "KYLOREN",
+    power: 27448,
+  },
+  {
+    base_id: "PHASMA",
+    power: 24033,
+  },
+  {
+    base_id: "FIRSTORDEREXECUTIONER",
+    power: 21052,
+  },
+  {
+    base_id: "SMUGGLERHAN",
+    power: 21984,
+  },
+  {
+    base_id: "FOSITHTROOPER",
+    power: 24033,
+  },
+  {
+    base_id: "FIRSTORDERSPECIALFORCESPILOT",
+    power: 19003,
+  },
+  {
+    base_id: "GENERALHUX",
+    power: 24817,
+  },
+  {
+    base_id: "FIRSTORDERTIEPILOT",
+    power: 21199,
+  },
+  {
+    base_id: "EMPERORPALPATINE",
+    power: 30429,
+  },
+
+  {
+    base_id: "CAPITALFINALIZER",
+    power: 30300,
+  },
+];
+
+const RAY_REQ_UNITS = [
+  {
+    base_id: "REYJEDITRAINING",
+    power: 33409,
+  },
+  {
+    base_id: "FINN",
+    power: 24033,
+  },
+  {
+    base_id: "RESISTANCETROOPER",
+    power: 20842,
+  },
+  {
+    base_id: "REY",
+    power: 27448,
+  },
+  {
+    base_id: "RESISTANCEPILOT",
+    power: 18793,
+  },
+  {
+    base_id: "POE",
+    power: 21627,
+  },
+  {
+    base_id: "EPIXFINN",
+    power: 24033,
+  },
+  {
+    base_id: "AMILYNHOLDO",
+    power: 24033,
+  },
+  {
+    base_id: "ROSETICO",
+    power: 24033,
+  },
+  {
+    base_id: "EPIXPOE",
+    power: 24033,
+  },
+  {
+    base_id: "BB8",
+    power: 30429,
+  },
+  {
+    base_id: "SMUGGLERCHEWBACCA",
+    power: 21984,
+  },
+
+  {
+    base_id: "CAPITALRADDUS",
+    power: 30000,
+  },
+];
+
+module.exports = {
+  KAYLO_REQ_UNITS,
+  RAY_REQ_UNITS,
+};
--- server/controller/legendCharacterController.js
+++ server/controller/legendCharacterController.js
@@ -1,126 +1,16 @@
-const loadData = require ('../../server/RequestsforGG');
-const brazzers = require('../../data/brazzers');
-const fs = require('fs');
-const KAYLO_REQ_UNITS = [
-	{
-		base_id: 'KYLORENUNMASKED',
-		power: 30429
-	},
-	{
-		base_id: 'FIRSTORDERTROOPER',
-		power: 23248
-	},
-	{
-		base_id: 'FIRSTORDEROFFICERMALE',
-		power: 21052
-	},
-	{
-		base_id: 'KYLOREN',
-		power: 27448
-	},
-	{
-		base_id: 'PHASMA',
-		power: 24033
-	},
-	{
-		base_id: 'FIRSTORDEREXECUTIONER',
-		power: 21052
-	},
-	{
-		base_id: 'SMUGGLERHAN',
-		power: 21984
-	},
-	{
-		base_id: 'FOSITHTROOPER',
-		power: 24033
-	},
-	{
-		base_id: 'FIRSTORDERSPECIALFORCESPILOT',
-		power: 19003
-	},
-	{
-		base_id: 'GENERALHUX',
-		power: 24817
-	},
-	{
-		base_id: 'FIRSTORDERTIEPILOT',
-		power: 21199
-	},
-	{
-		base_id: 'EMPERORPALPATINE',
-		power: 30429
-	},
-
-	{
-		base_id: 'CAPITALFINALIZER',
-		power: 30300
-	},
-
-];
-
-const RAY_REQ_UNITS = [
-	{
-		base_id: 'REYJEDITRAINING',
-		power: 33409
-	},
-	{
-		base_id: 'FINN',
-		power: 24033
-	},
-	{
-		base_id: 'RESISTANCETROOPER',
-		power: 20842
-	},
-	{
-		base_id: 'REY',
-		power: 27448
-	},
-	{
-		base_id: 'RESISTANCEPILOT',
-		power: 18793
-	},
-	{
-		base_id: 'POE',
-		power: 21627
-	},
-	{
-		base_id: 'EPIXFINN',
-		power: 24033
-	},
-	{
-		base_id: 'AMILYNHOLDO',
-		power: 24033
-	},
-	{
-		base_id: 'ROSETICO',
-		power: 24033
-	},
-	{
-		base_id: 'EPIXPOE',
-		power: 24033
-	},
-	{
-		base_id: 'BB8',
-		power: 30429
-	},
-	{
-		base_id: 'SMUGGLERCHEWBACCA',
-		power: 21984
-	},
-
-	{
-		base_id: 'CAPITALRADDUS',
-		power: 30000
-	},
-
-];
-
+const loadData = require("../../server/RequestsforGG");
+const brazzers = require("../../data/brazzers");
+const fs = require("fs");
+const {
+  RAY_REQ_UNITS,
+  KAYLO_REQ_UNITS,
+} = require("../../data/requirementsForUnit");
 
 module.exports = legendCharacterController = {
 	checkGuild: async function(guild = brazzers){
 		const result = [];
-		for (let i = 0; i < brazzers.length; i++) {
-			const player = brazzers[i];
+		for (let i = 0; i < guild.length; i++) {
+			const player = guild[i];
 			const units = (await loadData.getPlayer(player.id)).units;
 			const mods = await loadData.getAllMods(player.id);
 			const kyloProgress = await legendCharacterController.playerProgressToLegend(units, mods, KAYLO_REQ_UNITS);
@@ -164,13 +54,13 @@ module.exports = legendCharacterController = {
 };
 
 function sortByKylo(first, second) {
-	return second.kyloProgress - first.kyloProgress;
+  return second.kyloProgress - first.kyloProgress;
 }
 
 function sortByRey(first, second) {
-	return second.reyProgress - first.reyProgress;
+  return second.reyProgress - first.reyProgress;
 }
 
 function sortByName(first, second) {
-	return second.name - first.name;
+  return second.name - first.name;
 }
